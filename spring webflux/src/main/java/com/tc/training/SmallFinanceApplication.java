23c1833ae2001b5f99998a61fde4aabdf1eda04b
AnishTC <anish.marpalli@thoughtclan.com>
2023-10-11 16:07:11 +0530

    updated flux

--- spring webflux/src/main/java/com/tc/training/SmallFinanceApplication.java
+++ spring webflux/src/main/java/com/tc/training/SmallFinanceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
-public class SmallFinanceApplication {
+public class SmallFinanceApplication {//hello
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmallFinanceApplication.class, args);
