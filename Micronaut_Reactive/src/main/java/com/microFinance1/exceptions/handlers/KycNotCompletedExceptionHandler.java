cf2f73c762ae4a5334331c8ee1643368fa887a98
Anvith <anvith.gmail.com>
2023-11-06 11:55:08 +0530

    updated

--- Micronaut_Reactive/src/main/java/com/microFinance1/exceptions/handlers/KycNotCompletedExceptionHandler.java
+++ Micronaut_Reactive/src/main/java/com/microFinance1/exceptions/handlers/KycNotCompletedExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.microFinance1.exceptions.handlers;
 
 import com.microFinance1.exceptions.KycNotCompletedException;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.Order;
+import io.micronaut.core.order.Ordered;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
-
+//@Requires(classes = { KycNotCompletedException.class, ExceptionHandler.class })
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class KycNotCompletedExceptionHandler implements ExceptionHandler<KycNotCompletedException,String > {
     @Override
     public String handle(HttpRequest request, KycNotCompletedException exception) {
--- Micronaut_Reactive/target/generated-sources/annotations/com/microFinance1/mapper/AccountDetailsMapperImpl.java
+++ Micronaut_Reactive/target/generated-sources/annotations/com/microFinance1/mapper/AccountDetailsMapperImpl.java
@@ -9,7 +9,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-31T19:06:29+0530",
+    date = "2023-11-06T10:56:47+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17 (Oracle Corporation)"
 )
 @Singleton
