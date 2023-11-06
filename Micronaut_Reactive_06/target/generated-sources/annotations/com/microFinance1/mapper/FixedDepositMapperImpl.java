b8df7a6b32e9e376f786e7d9be62ea658b721ce2
pavan-thoughtclan <pavan.s.gowda@thoughtclan.com>
2023-11-06 12:25:26 +0530

    target file uploded

---
+++ Micronaut_Reactive_06/target/generated-sources/annotations/com/microFinance1/mapper/FixedDepositMapperImpl.java
@@ -0,0 +1,68 @@
+package com.microFinance1.mapper;
+
+import com.microFinance1.dtos.inputs.FixedDepositInputDto;
+import com.microFinance1.dtos.inputs.TransactionInputDto;
+import com.microFinance1.dtos.outputs.FixedDepositOutputDto;
+import com.microFinance1.entities.FixedDeposit;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-31T19:06:29+0530",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17 (Oracle Corporation)"
+)
+@Singleton
+@Named
+public class FixedDepositMapperImpl implements FixedDepositMapper {
+
+    @Override
+    public FixedDepositOutputDto mapToFixedDepositOutputDto(FixedDeposit fixedDeposit) {
+        if ( fixedDeposit == null ) {
+            return null;
+        }
+
+        FixedDepositOutputDto fixedDepositOutputDto = new FixedDepositOutputDto();
+
+        fixedDepositOutputDto.setFdId( fixedDeposit.getId() );
+        fixedDepositOutputDto.setInterestRate( fixedDeposit.getInterestRate() );
+        fixedDepositOutputDto.setInterestAmountAdded( fixedDeposit.getInterestAmount() );
+        fixedDepositOutputDto.setAmount( fixedDeposit.getAmount() );
+        fixedDepositOutputDto.setIsActive( fixedDeposit.getIsActive() );
+        fixedDepositOutputDto.setAccountNumber( fixedDeposit.getAccountNumber() );
+        fixedDepositOutputDto.setMaturityDate( fixedDeposit.getMaturityDate() );
+        fixedDepositOutputDto.setTotalAmount( fixedDeposit.getTotalAmount() );
+
+        return fixedDepositOutputDto;
+    }
+
+    @Override
+    public FixedDeposit mapToFixedDeposit(FixedDepositInputDto fixedDepositInputDto) {
+        if ( fixedDepositInputDto == null ) {
+            return null;
+        }
+
+        FixedDeposit fixedDeposit = new FixedDeposit();
+
+        fixedDeposit.setAccountNumber( fixedDepositInputDto.getAccountNumber() );
+        fixedDeposit.setAmount( fixedDepositInputDto.getAmount() );
+
+        return fixedDeposit;
+    }
+
+    @Override
+    public TransactionInputDto FixedDepositToTransactionInputDto(FixedDeposit fd) {
+        if ( fd == null ) {
+            return null;
+        }
+
+        TransactionInputDto transactionInputDto = new TransactionInputDto();
+
+        transactionInputDto.setTo( fd.getAccountNumber() );
+        transactionInputDto.setAccountNumber( fd.getAccountNumber() );
+        transactionInputDto.setAmount( fd.getAmount() );
+
+        return transactionInputDto;
+    }
+}
