702e46636d7fae45f3add9f3379224d006aaa694
pavan-thoughtclan <pavan.s.gowda@thoughtclan.com>
2023-11-06 12:07:49 +0530

    updated

---
+++ Smallfinance_spring_mvc/target/generated-sources/annotations/com/tc/training/smallfinance/mapper/LoanMapperImpl.java
@@ -0,0 +1,39 @@
+package com.tc.training.smallfinance.mapper;
+
+import com.tc.training.smallfinance.dtos.outputs.LoanOutputDto;
+import com.tc.training.smallfinance.model.Loan;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-06T11:52:14+0530",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
+)
+@Component
+public class LoanMapperImpl implements LoanMapper {
+
+    @Override
+    public LoanOutputDto mapToLoanOutputDto(Loan loan) {
+        if ( loan == null ) {
+            return null;
+        }
+
+        LoanOutputDto loanOutputDto = new LoanOutputDto();
+
+        loanOutputDto.setLoanId( loan.getId() );
+        loanOutputDto.setAccountNumber( map( loan.getAccountNumber() ) );
+        loanOutputDto.setAppliedDate( loan.getAppliedDate() );
+        loanOutputDto.setIsActive( loan.getIsActive() );
+        loanOutputDto.setStatus( loan.getStatus() );
+        loanOutputDto.setLoanedAmount( loan.getLoanedAmount() );
+        loanOutputDto.setTypeOfLoan( loan.getTypeOfLoan() );
+        loanOutputDto.setLoanEndDate( loan.getLoanEndDate() );
+        loanOutputDto.setInterest( loan.getInterest() );
+        loanOutputDto.setInterestAmount( loan.getInterestAmount() );
+        loanOutputDto.setMonthlyInterestAmount( loan.getMonthlyInterestAmount() );
+        loanOutputDto.setTotalAmount( loan.getTotalAmount() );
+
+        return loanOutputDto;
+    }
+}
---
+++ Smallfinance_spring_mvc/target/generated-sources/annotations/com/tc/training/smallfinance/mapper/TransactionMapperImpl.java
@@ -0,0 +1,50 @@
+package com.tc.training.smallfinance.mapper;
+
+import com.tc.training.smallfinance.dtos.inputs.TransactionInputDto;
+import com.tc.training.smallfinance.dtos.outputs.TransactionOutputDto;
+import com.tc.training.smallfinance.model.Transaction;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-06T11:52:14+0530",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
+)
+@Component
+public class TransactionMapperImpl implements TransactionMapper {
+
+    @Override
+    public Transaction mapToTransaction(TransactionInputDto transactionInputDto) {
+        if ( transactionInputDto == null ) {
+            return null;
+        }
+
+        Transaction transaction = new Transaction();
+
+        transaction.setAmount( transactionInputDto.getAmount() );
+
+        transaction.setTo( mapToAccountDetails(transactionInputDto) );
+
+        return transaction;
+    }
+
+    @Override
+    public TransactionOutputDto mapToTransactionOutputDto(Transaction transaction) {
+        if ( transaction == null ) {
+            return null;
+        }
+
+        TransactionOutputDto transactionOutputDto = new TransactionOutputDto();
+
+        transactionOutputDto.setTransactionID( transaction.getId() );
+        transactionOutputDto.setAmount( transaction.getAmount() );
+        transactionOutputDto.setTransactionType( transaction.getTransactionType() );
+        transactionOutputDto.setTimestamp( transaction.getTimestamp() );
+        transactionOutputDto.setWhichTransaction( transaction.getWhichTransaction() );
+        transactionOutputDto.setBalance( transaction.getBalance() );
+        transactionOutputDto.setDescription( transaction.getDescription() );
+
+        return transactionOutputDto;
+    }
+}
